f55d57584ddbd7c1f327358bb114dff883b21385
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-04-24 12:07:51 +0300

    .

--- lib/home.dart
+++ lib/home.dart
@@ -6,9 +6,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("hello"),
-      ),
+      body: HomeBody(),
     );
   }
 }
+
+
+class HomeBody extends StatefulWidget {
+  const HomeBody({super.key});
+
+  @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          SizedBox(height: 20,),
+          Row(
+            children: [
+              Icon(Icons.sunny,color: Colors.green,),
+              SizedBox(width: 5,),
+              Text("GOOD MORNİNG",style: TextStyle(color: Colors.green,fontSize: 16))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+
+
+
+
+  Row getDayTime(){
+
+
+
+    return Row(
+      children: [],
+    );
+  }
+}
+
